0cd76d020c9e4f5ec5058a0f2e3c7c172125ac03
Karim Alibhai <karim@alibhai.co>
2016-11-23 13:29:04 -0500

    switch from npm aliases to yarn

---
+++ aliases/yarn.sh
@@ -0,0 +1,11 @@
+## awesome yarn aliases
+
+alias n="yarn"
+alias i="n i"
+alias is="n add"
+alias id="is -D"
+alias ig="n global add"
+alias u="n publish"
+alias nr="n run"
+alias ns="nr start"
+alias nt="nr test"
