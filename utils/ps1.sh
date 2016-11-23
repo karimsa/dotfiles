7371b48d2efa276d27bf46b955d4e4ef328ecd21
Karim Alibhai <karim@alibhai.co>
2016-11-23 13:12:24 -0500

    add terminator to parse_git_branch

--- utils/ps1.sh
+++ utils/ps1.sh
@@ -1,5 +1,5 @@
 # Sexy bash prompt
 # Includes custom character for the prompt, path, and Git branch name.
 # Source: https://github.com/mdo/config/blob/master/.bash_profile
-parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' }
+parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'; }
 export PS1="\n\[$(tput bold)\]\[$(tput setaf 5)\]➜ \[$(tput setaf 6)\]\${PWD##*/}\[$(tput setaf 3)\]\$(parse_git_branch) \[$(tput sgr0)\]"
