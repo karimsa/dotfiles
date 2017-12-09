72515debe4c5109040499f627db0244a753ed679
Karim Alibhai <karim@foko.co>
2017-12-09 03:20:11 -0500

    Stuff

--- aliases/git.sh
+++ aliases/git.sh
@@ -12,3 +12,8 @@ alias po="p origin"
 alias yo="y origin"
 alias pm="po \$(git rev-parse --abbrev-ref HEAD)"
 alias ym="yo \$(git rev-parse --abbrev-ref HEAD)"
+
+yk () {
+  git clone git@github.com:${1}.git ${2}
+}
+
