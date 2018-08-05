2a58f6b2e877aa093c441fe60d5ff7fb49cf40e6
Karim Alibhai <karim@foko.co>
2018-08-05 11:51:27 -0400

    Add: bw util for grepping in between

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -10,3 +10,11 @@ alias mute="osascript -e 'set volume output muted true'"
 alias unmute="osascript -e 'set volume output muted false'"
 alias nstat="netstat -tulnp"
 
+function bw() {
+  if test -z "$1" || test -z "$2"; then
+    echo "usage: bw <pttn-1> <pttn-2>"
+    return
+  fi
+  awk "/$1/{flag=1;next}/$2/{flag=0}flag"
+}
+
