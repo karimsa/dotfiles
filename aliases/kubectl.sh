74e2237f1d91022a5646eba721fb3bd6e5450168
Karim Alibhai <karim.alibhai@acuitymd.com>
2025-09-05 10:11:49 -0400

    fix k8s aliases

--- aliases/kubectl.sh
+++ aliases/kubectl.sh
@@ -1,13 +1,28 @@
 alias k="kubectl"
-alias kx="kubectx"
-alias kn="kubens"bn
-alias kprd="kubectx core-prd"
-alias kstg="kubectx core-stg"
-alias kdev="kubectx core-dev"
-alias kcare="kubens carejourney"
 alias kpods="kubectl get pods"
+alias kpickpod="k get pods -o json --field-selector=status.phase=Running | jq '.items[].metadata.name' -r | fzf"
 
-alias kpickpod="k get pods -o json | jq '.items[].metadata.name' -r | fzf"
+kctx() {
+	query="$1"
+
+	if test -z "$query"; then
+		kubectx
+		return
+	fi
+
+	kubectx `kubectl config get-contexts --output=name | fzf --query="$query" --select-1`
+}
+
+kns() {
+	query="$1"
+
+	if test -z "$query"; then
+		kubens
+		return
+	fi
+
+	kubens `kubectl get namespaces --output name | sed 's/namespace\///' | fzf --query="$query" --select-1`
+}
 
 kpickcontainer() {
 	k get pod "${1}" -o json | jq '.spec.containers[].name' -r | fzf --select-1
